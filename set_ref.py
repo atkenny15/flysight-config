b5c489a91441a36094e6afb51fc5fb48294aa68a
Andy Kenny <atkenny15@gmail.com>
2023-05-25 23:56:25 -0400

    Improve set_ref.py

--- set_ref.py
+++ set_ref.py
@@ -15,6 +15,7 @@ def main(argv: List[str]) -> None:
     parser.add_argument(
         "name",
         default=None,
+        nargs="?",
         type=str,
         help="delete messages where From matches regex",
     )
@@ -36,19 +37,22 @@ def main(argv: List[str]) -> None:
         split = args.lat_lon.split(",")
         if len(split) != 2:
             raise Exception(f"Invalid lat/lon: '{args.lat_lon}'")
-        lat = int(round(float(split[0].strip()) * scale))
-        lon = int(round(float(split[1].strip()) * scale))
+        lat = float(split[0].strip())
+        lon = float(split[1].strip())
 
     config_path = FILE_PATH / "CONFIG.TXT"
     with config_path.open() as fh:
         lines = fh.readlines()
 
-    ref_rx = re.compile(";\s*ref_name\s*:\s+(.+?)\s*$")
-    lat_lon_rx = re.compile("^\s*;?\s*Reference_(Lat|Lon)\s*:\s*(-?\d+)(?:\s+.*?)?$")
+    ref_rx = re.compile(";\s*ref_name\s*:\s+(?P<name>.+?)\s*$")
+    lat_lon_rx = re.compile(
+        "^\s*(?P<disabled>;)?\s*Reference_(?P<lat_lon>Lat|Lon)F?\s*:\s*(?P<value>-?[\d.]+)(?:\s+.*?)?$"
+    )
 
     found = False
     last_ref = -1
 
+    is_first = True
     new_lines = []
     i = 0
     while i < len(lines):
@@ -58,68 +62,96 @@ def main(argv: List[str]) -> None:
         i += 1
 
         if name_matches:
-            print(line)
+            disp = [line]
 
-            name_match = name_matches.group(1) == args.name
-            if name_match:
-                found = True
+            name_match = None
+            if args.name is not None:
+                name_match = name_matches.group("name") == args.name
+                if name_match:
+                    found = True
 
+            is_disabled = False
             while i < len(lines):
                 lat_lon_matches = lat_lon_rx.search(lines[i])
 
                 if not lat_lon_matches:
                     break
 
-                s = ""
-                if not name_match:
-                    s = ";"
+                if name_match is None:
+                    s = ""
+                    if lat_lon_matches.group("disabled"):
+                        s = ";"
+                else:
+                    s = ""
+                    if not name_match:
+                        s = ";"
 
-                val = int(lat_lon_matches.group(2))
+                is_disabled = False
+                if s == ";":
+                    is_disabled = True
+
+                val = lat_lon_matches.group("value")
 
                 if name_match:
-                    if lat_lon_matches.group(1) == "Lat":
+                    if lat_lon_matches.group("lat_lon") == "Lat":
                         if lat is not None:
                             val = lat
-                    if lat_lon_matches.group(1) == "Lon":
+                    if lat_lon_matches.group("lat_lon") == "Lon":
                         if lon is not None:
                             val = lon
 
-                temp = "{}Reference_{}: {:10}".format(s, lat_lon_matches.group(1), val)
+                temp = "{}Reference_{}F: {}".format(
+                    s, lat_lon_matches.group("lat_lon"), val
+                )
                 new_lines.append(temp)
-                print(temp)
+                disp.append(temp)
 
                 i += 1
 
             last_ref = len(new_lines)
 
-    if not found:
-        if lat is None or lon is None:
-            raise Exception(
-                f"Missing lat/lon argument for new reference point '{args.name}'"
-            )
+            if not is_first:
+                print()
+            is_first = False
 
-        if last_ref < 0:
-            raise Exception("Could not find any reference points")
+            for s in disp:
+                if is_disabled:
+                    print(s)
+                else:
+                    print(f"\x1b[36m{s}\x1b[0m")
 
-        backup = new_lines
-        new_lines = []
+    if args.name is not None:
+        if not found:
+            if lat is None or lon is None:
+                raise Exception(
+                    f"Missing lat/lon argument for new reference point '{args.name}'"
+                )
 
-        i = 0
-        while i < len(backup):
-            new_lines.append(backup[i])
-            i += 1
-            if i == last_ref:
-                add = [
-                    "",
-                    f"; ref_name: {args.name}",
-                    f"Reference_Lat: {lat:10}",
-                    f"Reference_Lon: {lon:10}",
-                ]
-                print("\n".join(add))
-                new_lines.extend(add)
+            if last_ref < 0:
+                raise Exception("Could not find any reference points")
 
-    with config_path.open("w") as fh:
-        fh.write("\n".join(new_lines) + "\n")
+            backup = new_lines
+            new_lines = []
+
+            i = 0
+            while i < len(backup):
+                new_lines.append(backup[i])
+                i += 1
+                if i == last_ref:
+                    add = [
+                        "",
+                        f"; ref_name: {args.name}",
+                        f"Reference_Lat: {lat:10}",
+                        f"Reference_Lon: {lon:10}",
+                    ]
+                    print("\n".join(add))
+                    new_lines.extend(add)
+
+        print("\n\x1b[32;1mUpdating config file\x1b[0m")
+        with config_path.open("w") as fh:
+            fh.write("\n".join(new_lines) + "\n")
+    else:
+        print("\n\x1b[31;1mSkipping config file\x1b[0m")
 
 
 if __name__ == "__main__":
